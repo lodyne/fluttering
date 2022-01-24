72f25139e00f4bafd2d4793ed62448c3eb62721a
lodyne <lodgmtui@gmail.com>
2022-01-24 15:48:12 +0300

    feat(image): add image using NetworkImage
    ~ image from internet using url

--- newApp/lib/main.dart
+++ newApp/lib/main.dart
@@ -25,6 +25,12 @@ class HomePage extends StatelessWidget {
               ),
           ) ,
           backgroundColor: Colors.lightBlue,
+        
+        ),
+        body: const Center(
+          child: Image(
+          image:NetworkImage('https://images.unsplash.com/photo-1628126235206-5260b9ea6441?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80'),
+          ),
         ),
       backgroundColor: Colors.pink,
       ),    
