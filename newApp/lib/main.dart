a18237ce0cb83462673adc0f5684a4a70791d029
lodyne <lodgmtui@gmail.com>
2022-01-24 16:52:46 +0300

    feat(image,fonts): add new image and font
    ~ add new image using AssetImage by creating image folder
    ~ add fonts using Google Fonts and updating pubspec.yaml file

--- newApp/lib/main.dart
+++ newApp/lib/main.dart
@@ -19,7 +19,8 @@ class HomePage extends StatelessWidget {
               style: TextStyle(
                 fontSize: 30.0,
                 fontStyle: FontStyle.italic,
-                letterSpacing: 1.2,
+                fontFamily:'IndieFlower',
+                letterSpacing: 0.5,
                 color: Colors.pink,
               ),
               ),
@@ -29,7 +30,7 @@ class HomePage extends StatelessWidget {
         ),
         body: const Center(
           child: Image(
-          image:NetworkImage('https://images.unsplash.com/photo-1628126235206-5260b9ea6441?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80'),
+          image:AssetImage('images/bestyou.jpg'),
           ),
         ),
       backgroundColor: Colors.pink,
