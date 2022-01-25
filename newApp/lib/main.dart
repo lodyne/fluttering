b18c507dd84ce5d65dfa38e67815012e4cffcd26
lodyne <lodgmtui@gmail.com>
2022-01-25 15:56:48 +0300

    feat(container) : use container Widget

--- newApp/lib/main.dart
+++ newApp/lib/main.dart
@@ -32,35 +32,42 @@ class HomePage extends StatelessWidget {
         //     image: AssetImage('images/bestyou.jpg'),
         //   ),
         // ),
-        body: Center(
-          child: ElevatedButton.icon(
-            onPressed: () {
-              print('clicked');
-            },
-            icon: const Icon(
-              Icons.home,
-              color: Colors.indigo,
-              size: 50.0,
-            ),
-            label: const Text(
-              "HomeComing",
-              style: TextStyle(
-                fontSize: 30.0,
-                fontStyle: FontStyle.italic,
-                fontFamily: 'IndieFlower',
-                letterSpacing: 0.5,
-                color: Colors.pink,
-              ),
-            ),
-          ),
+        body: Container(
+          color: Colors.amber,
+          padding: const EdgeInsets.fromLTRB(20, 10, 20, 30),
+          margin: const EdgeInsets.symmetric(horizontal: 170, vertical: 250),
+          child: const Text('hello'),
+
+          // child: ElevatedButton.icon(
+          //   onPressed: () {
+          //     print('clicked');
+          //   },
+          //   icon: const Icon(
+          //     Icons.home,
+          //     color: Colors.indigo,
+          //     size: 50.0,
+          //   ),
+          //   label: const Text(
+          //     "HomeComing",
+          //     style: TextStyle(
+          //       fontSize: 30.0,
+          //       fontStyle: FontStyle.italic,
+          //       fontFamily: 'IndieFlower',
+          //       letterSpacing: 0.5,
+          //       color: Colors.pink,
+          //     ),
+          //   ),
+          // ),
         ),
-        backgroundColor: Colors.pink,
+        // backgroundColor: Colors.pink,
         floatingActionButton: FloatingActionButton(
           onPressed: () {},
           child: IconButton(
               onPressed: () {},
+              color: Colors.lightBlueAccent,
               icon: const Icon(
                 Icons.add,
+                color: Colors.pink,
               )),
         ),
       ),
