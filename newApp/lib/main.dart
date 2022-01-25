a02f58a94f1efb6af4d02b8abf8462787cd040de
lodyne <lodgmtui@gmail.com>
2022-01-25 12:25:03 +0300

    feat(button,icon):add button and icon

--- newApp/lib/main.dart
+++ newApp/lib/main.dart
@@ -5,7 +5,7 @@ void main(List<String> args) {
 }
 
 class HomePage extends StatelessWidget {
-  const HomePage({ Key? key }) : super(key: key);
+  const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -13,28 +13,51 @@ class HomePage extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
-          title: const Center( 
+          title: const Center(
             child: Text(
               'Photo Album',
               style: TextStyle(
                 fontSize: 30.0,
                 fontStyle: FontStyle.italic,
-                fontFamily:'IndieFlower',
+                fontFamily: 'IndieFlower',
                 letterSpacing: 0.5,
                 color: Colors.pink,
               ),
-              ),
-          ) ,
-          backgroundColor: Colors.lightBlue,
-        
-        ),
-        body: const Center(
-          child: Image(
-          image:AssetImage('images/bestyou.jpg'),
+            ),
           ),
+          backgroundColor: Colors.lightBlue,
         ),
-      backgroundColor: Colors.pink,
-      ),    
+        // body: const Center(
+        //   child: Image(
+        //     image: AssetImage('images/bestyou.jpg'),
+        //   ),
+        // ),
+        body: Center(
+          child: TextButton(
+              onPressed: () {
+                print('clicked');
+              },
+              child: const Icon(
+                Icons.home,
+                color: Colors.indigo,
+                size: 50.0,
+              )),
+        ),
+        backgroundColor: Colors.pink,
+        // bottomNavigationBar: BottomNavigationBar(
+        //   items: const [
+        //     BottomNavigationBarItem(
+        //         icon: Icon(
+        //       Icons.home,
+        //     )),
+        //     BottomNavigationBarItem(
+        //       icon: Icon(
+        //         Icons.search,
+        //       ),
+        //     )
+        //   ],
+        // ),
+      ),
     );
   }
 }
